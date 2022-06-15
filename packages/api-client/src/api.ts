7ca7aa77b9ce0424dcaf7c375b7c77f0798438a5
Stefan Jokic <s.jokic@beeit.rs>
2022-06-15 12:10:45 +0200

    init

--- packages/api-client/src/api.ts
+++ packages/api-client/src/api.ts
@@ -3,9 +3,13 @@ const client = algoliasearch('MN8I8UO0AC', '48e14f065359d34167f565f01c453bf1');
 const products = client.initIndex('magento2_devdefault_products');
 const categories = client.initIndex('magento2_devdefault_categories');
 
-const searchProducts = async (context: any, params: any): Promise<any> => {
+interface Params {
+    query: string
+}
+
+const searchProducts = async (context: object, params: Params): Promise<any> => {
   try {
-    const { query } = params
+    const { query } = params;
     const response = await products.search(query, { hitsPerPage: 20 });
     return response;
   } catch (error) {
@@ -13,9 +17,9 @@ const searchProducts = async (context: any, params: any): Promise<any> => {
   }
 };
 
-const searchCategories = async (context: any, params: any): Promise<any> => {
+const searchCategories = async (context: object, params: Params): Promise<any> => {
   try {
-    const { query } = params
+    const { query } = params;
     const response = await categories.search(query, { hitsPerPage: 5 });
     return response;
   } catch (error) {
@@ -25,5 +29,5 @@ const searchCategories = async (context: any, params: any): Promise<any> => {
 
 export default {
   searchProducts,
-  searchCategories,
+  searchCategories
 };
