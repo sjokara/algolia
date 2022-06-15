e2e2c06af5818e775c1f10daeb0f4b1db9a42f4e
Stefan Jokic <s.jokic@beeit.rs>
2022-06-15 16:14:58 +0200

    update

--- packages/api-client/lib/api.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    searchProducts: (context: object, params: string) => Promise<any>;
-    searchCategories: (context: object, params: string) => Promise<any>;
-};
-export default _default;
