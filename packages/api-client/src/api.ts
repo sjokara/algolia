f1b92057558825a4020f23cda37a93b251127ca9
Stefan Jokic <s.jokic@beeit.rs>
2022-06-15 14:55:18 +0200

    add lib files

--- packages/api-client/src/api.ts
+++ packages/api-client/src/api.ts
@@ -3,7 +3,6 @@ const client = algoliasearch('MN8I8UO0AC', '48e14f065359d34167f565f01c453bf1');
 const products = client.initIndex('magento2_devdefault_products');
 const categories = client.initIndex('magento2_devdefault_categories');
 
-
 const searchProducts = async (context: object, params: string): Promise<any> => {
   try {
     const response = await products.search(params, { hitsPerPage: 20 });
