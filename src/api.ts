5b225cdc8aef21a9f15a65ebbc25bac756cf301b
Stefan Jokic <s.jokic@beeit.rs>
2022-06-16 12:51:12 +0200

    configure tsconfig, package json and script for generating lib files

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
-const algoliasearch = require('algoliasearch');
-const client = algoliasearch('MN8I8UO0AC', '48e14f065359d34167f565f01c453bf1');
-const products = client.initIndex('magento2_devdefault_products');
-const categories = client.initIndex('magento2_devdefault_categories');
+const algoliasearch = require("algoliasearch");
+const client = algoliasearch("MN8I8UO0AC", "48e14f065359d34167f565f01c453bf1");
+const products = client.initIndex("magento2_devdefault_products");
+const categories = client.initIndex("magento2_devdefault_categories");
 
 const searchProducts = async (context: object, params: string): Promise<any> => {
   try {
@@ -21,7 +21,33 @@ const searchCategories = async (context: object, params: string): Promise<any> =
   }
 };
 
+const searchMultiple = async (context: object, params: string): Promise<any> => {
+  try {
+    const queries = [
+      {
+        indexName: "magento2_devdefault_categories",
+        query: params,
+        params: {
+          hitsPerPage: 5,
+        },
+      },
+      {
+        indexName: "magento2_devdefault_products",
+        query: params,
+        params: {
+          hitsPerPage: 10,
+        },
+      },
+    ];
+    const response = await client.multipleQueries(queries);
+    return response;
+  } catch (error) {
+    return [];
+  }
+};
+
 export default {
   searchProducts,
-  searchCategories
+  searchCategories,
+  searchMultiple
 };
