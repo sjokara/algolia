671e0b4eb015145ed2af606d8bed1c70cc120d1b
Stefan Jokic <s.jokic@beeit.rs>
2022-06-16 13:07:08 +0200

    init

--- lib/api.d.ts
+++ lib/api.d.ts
@@ -2,5 +2,7 @@ declare const _default: {
     searchProducts: (context: object, params: string) => Promise<any>;
     searchCategories: (context: object, params: string) => Promise<any>;
     searchMultiple: (context: object, params: string) => Promise<any>;
+    getProductsByCategory: (context: object, params: any) => Promise<any>;
+    getRelatedProducts: (context: object, params: any) => Promise<any>;
 };
 export default _default;
